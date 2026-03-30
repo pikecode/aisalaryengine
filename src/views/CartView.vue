8086fa688e219efd1f34863acd74ede863a34f0e
pikecode <pikecode@gmail.com>
2026-03-30 22:46:00 +0800

    feat: scaffold CartView and CartViewPC placeholder

---
+++ src/views/CartView.vue
@@ -0,0 +1,7 @@
+<template>
+  <CartViewPC />
+</template>
+
+<script setup lang="ts">
+import CartViewPC from './cart/CartViewPC.vue'
+</script>
---
+++ src/views/cart/CartViewPC.vue
@@ -0,0 +1,3 @@
+<template>
+  <div>购物车 PC（待实现）</div>
+</template>
